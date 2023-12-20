7b41c485d52c282340f7cd1b754c54efe2d56090
Gleb Zavalov <oldg590@gmail.com>
2023-12-20 16:50:11 +0300

    fix eventCard styles

--- components/shared/EventCard.tsx
+++ components/shared/EventCard.tsx
@@ -7,13 +7,15 @@ import { EventCardProps } from '@/types'
 const EventCard = ({ event }: EventCardProps) => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
-      <Image
-        src={event.imageUrl}
-        alt='hero image'
-        width={1000}
-        height={1000}
-        className='h-full min-h-[300px] object-cover object-center'
-      />
+      <div className='h-full min-h-[300px]'>
+        <Image
+          src={event.imageUrl}
+          alt='hero image'
+          width={1000}
+          height={1000}
+          className='h-full min-h-[300px] object-cover object-center'
+        />
+      </div>
 
       <div className='flex w-full flex-col gap-8 p-5 md:p-10'>
         <div className='flex flex-col gap-6'>
